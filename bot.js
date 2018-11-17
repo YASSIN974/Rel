eb06e3a71b4580ce08fdb241535e9f9b0f206736
YASSIN974 <44032576+YASSIN974@users.noreply.github.com>
2018-11-17 18:50:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', function(){
  game: { 
     type: 1,
      url: 'https://www.twitch.tv/skwadraa',
-    name: 'There is no place for love inside',
+    name: 'I do not want anything',
     application_id: '477187715658547201',
      assets: {
          large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
